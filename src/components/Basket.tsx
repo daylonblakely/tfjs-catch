26ae4920fe7e4c17e51114c2b970c3afae07a735
daylonblakely <daylonblakely@gmail.com>
2025-01-12 14:31:38 -0800

    added backboard

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -16,6 +16,44 @@ const Basket = () => {
 
   return (
     <div style={{ position: 'relative' }}>
+      {/* Backboard */}
+      <div
+        style={{
+          position: 'absolute',
+          top: BASKET_Y - 70, // Position the backboard slightly above the rim
+          left:
+            (x / HORIZONTAL_SECTIONS) *
+              (window.innerWidth - HORIZONTAL_SPACING * 2) +
+            HORIZONTAL_SPACING -
+            BASKET_OFFSET -
+            28, // Center the backboard relative to the rim
+          width: BASKET_WIDTH * 1.5, // Backboard is wider than the rim
+          height: BASKET_HEIGHT * 6,
+          backgroundColor: 'white',
+          border: '2px solid black',
+          borderRadius: '5px',
+          zIndex: -1, // Place the backboard behind the rim and net
+        }}
+      ></div>
+      <div
+        style={{
+          position: 'absolute',
+          top: BASKET_Y - 50, // Position the backboard slightly above the rim
+          left:
+            (x / HORIZONTAL_SECTIONS) *
+              (window.innerWidth - HORIZONTAL_SPACING * 2) +
+            HORIZONTAL_SPACING -
+            BASKET_OFFSET +
+            10, // Center the backboard relative to the rim
+          width: BASKET_WIDTH * 0.7, // Backboard is wider than the rim
+          height: BASKET_WIDTH / 2,
+          backgroundColor: 'white',
+          border: '4px solid red',
+          borderRadius: '5px',
+          zIndex: -1, // Place the backboard behind the rim and net
+        }}
+      ></div>
+
       {/* Rim */}
       <motion.div
         style={{
@@ -45,7 +83,7 @@ const Basket = () => {
             height: BASKET_HEIGHT * 2,
             backgroundImage: `repeating-linear-gradient(45deg, white 0, white 2px, transparent 2px, transparent 6px)`,
             backgroundSize: '10px 10px',
-            clipPath: 'polygon(5% 0, 95% 0, 80% 100%, 20% 100%)', // Trapezoid shape,
+            clipPath: 'polygon(5% 0, 95% 0, 80% 100%, 20% 100%)', // Trapezoid shape
           }}
         ></div>
       </motion.div>
